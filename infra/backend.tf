2acbe5e1bb523ee19efa9df0a5b8897eabc5ce8c
Abdi-Shakur457 <abdishakur457@hotmail.com>
2026-04-24 15:14:25 +0100

    adding required terraform providers to backend.tf file

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,4 +1,6 @@
 terraform {
+  required_version = ">= 1.7.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -11,7 +13,6 @@ terraform {
     }
   }
 }
-
 provider "cloudflare" {
   api_token = var.cloudflare_api_token
 }
